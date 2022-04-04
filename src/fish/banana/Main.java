c6c843a8e5745dd9c1dbc2af5d926117c7f62839
Oleg Petrov <83897251+TinyBananaFish@users.noreply.github.com>
2022-04-04 20:03:05 +0300

    Update Main.java

--- src/fish/banana/Main.java
+++ src/fish/banana/Main.java
@@ -3,11 +3,9 @@ package fish.banana;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hello, QA.GURU!");
         System.out.println("Another line");
     }
-	
     public static void another() {
         System.out.println("Another method!");
     }
