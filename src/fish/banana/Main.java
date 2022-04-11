8559905ed031dc0e6910ac6a1095bc16339747a0
TinyBananaFish <tinybananafish@gmail.com>
2022-04-11 11:57:08 +0300

    added slow branch

--- src/fish/banana/Main.java
+++ src/fish/banana/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, QA.GURU!");
         System.out.println("Another line");
+        System.out.println("slow branch");
+
     }
     public static void another() {
-        System.out.println("ANOTHER METHOD");
+        System.out.println("another method!");
     }
 }
